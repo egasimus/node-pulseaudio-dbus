07f0b17706dd1138ece9874448ded418a8cfd38e
Adam Avramov <fallenblood@gmail.com>
2014-11-30 16:01:10 +0200

    Finish cleaning up introspect.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var net       = require('net'),
-    dbus      = require('dbus-native'),
-    handshake = require('dbus-native/lib/handshake'),
-    events    = require('events');
+var
+  net       = require('net'),
+  dbus      = require('dbus-native'),
+  handshake = require('dbus-native/lib/handshake'),
+  events    = require('events');
 
-var detect = require('./lib/detect'),
-    client = require('./lib/client');
+var
+  detect = require('./lib/detect'),
+  Client = require('./lib/client');
 
-console.log("detect", detect.detectSync());
+var
+  pulse = new Client(detect.parse(detect.detectSync()));
 
-console.log("parse", detect.parse(detect.detectSync()));
+console.log("pulse", pulse);
 
-console.log(client(detect.parse(detect.detectSync())));
+console.log("iface", pulse.getInterface("org.PulseAudio.Core1"))
