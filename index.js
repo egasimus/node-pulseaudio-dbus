30b799d625831ba1913c8f1308f6e61e65dd1c4d
Adam Avramov <fallenblood@gmail.com>
2014-12-06 12:41:12 +0200

    Make main object more similar to its children.

--- index.js
+++ index.js
@@ -9,7 +9,17 @@ var trace = function () { console.log(arguments); }
 
 pulse.on(
   'NewPlaybackStream',
-  trace
+  function (path, args, sig) {
+    var
+      stream_path = args[0],
+      stream      = pulse.service.bus.getObject(
+        stream_path,
+        'org.PulseAudio.Core1.Stream',
+        function (err, res) {
+          console.log(pulse)
+          console.log(res);
+        });
+  }
 ).on(
   'PlaybackStreamRemoved',
   trace
